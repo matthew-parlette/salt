44cbe2f80e59e66122f6b575e9246783cdb5253c
matthew-parlette <matthew.parlette@gmail.com>
2016-01-06 09:53:37 -0500

    Removed config generation part of zsh script

--- shell/zsh/files/install.sh
+++ shell/zsh/files/install.sh
@@ -67,38 +67,38 @@ main() {
     fi
   fi
 
-  printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
-  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-    printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NORMAL}\n";
-    mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
-  fi
+  # printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
+  # if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    # printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NORMAL}\n";
+    # mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  # fi
 
-  printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
-  cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
-  sed "/^export ZSH=/ c\\
-  export ZSH=$ZSH
-  " ~/.zshrc > ~/.zshrc-omztemp
-  mv -f ~/.zshrc-omztemp ~/.zshrc
+  # printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
+  # cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
+  # sed "/^export ZSH=/ c\\
+  # export ZSH=$ZSH
+  # " ~/.zshrc > ~/.zshrc-omztemp
+  # mv -f ~/.zshrc-omztemp ~/.zshrc
 
-  printf "${BLUE}Copying your current PATH and adding it to the end of ~/.zshrc for you.${NORMAL}\n"
-  sed "/export PATH=/ c\\
-  export PATH=\"$PATH\"
-  " ~/.zshrc > ~/.zshrc-omztemp
-  mv -f ~/.zshrc-omztemp ~/.zshrc
+  # printf "${BLUE}Copying your current PATH and adding it to the end of ~/.zshrc for you.${NORMAL}\n"
+  # sed "/export PATH=/ c\\
+  # export PATH=\"$PATH\"
+  # " ~/.zshrc > ~/.zshrc-omztemp
+  # mv -f ~/.zshrc-omztemp ~/.zshrc
 
   # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  # TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  # if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # if hash chsh >/dev/null 2>&1; then
+      # printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+      # chsh -s $(grep /zsh$ /etc/shells | tail -1)
     # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
+    # else
+      # printf "I can't change your shell automatically because this system does not have chsh.\n"
+      # printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+    # fi
+  # fi
 
   printf "${GREEN}"
   echo '         __                                     __   '
